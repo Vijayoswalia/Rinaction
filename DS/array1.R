0546b14bd7d02037b38ac3aa906cc49fe014c9b2
vijay oswalia <vijayoswalia@gmail.com>
2017-08-25 14:30:29 +0530

    list,array,factor

--- DS/array1.R
+++ DS/array1.R
@@ -1,31 +1,31 @@
-company= c('coy1','coy2')
-department=c('dept','dept2','dept3')
-salesman=c('salesman1','salesman2','salesman3','salesman4')
-set.seed((1234))
-sales=ceiling(runif(2*3*4,50,100))
+# Arrays
+# 2 states ; Each State has 3 districts : Each District has 4 cities
+company = c('coy1', 'coy2')
+department = c('dept1','dept2', 'dept3')
+salesman = c('salesman1', 'salesman2', 'salesman3', 'salesman4')
+set.seed(1234)
+sales  = ceiling(runif(2*3*4,50,100))
 cat(sales)
-sales
-length(sales)
-mean(sales)
-sales;length(sales)
-?array
-salesarray=array(dim=c(4,3,2),data=sales,
-                 dimnames=list(salesman,department,company))
+sales; length(sales)
+salesarray = array(sales, c(4,3,2) , dimnames = list(salesman,department,company))
 salesarray
 colnames(salesarray)
 rownames(salesarray)
+
 dimnames(salesarray)
-dimnames(salesarray)[[3]]
 length(salesarray)
 nrow(salesarray)
 ncol(salesarray)
 dim(salesarray)
 class(salesarray)
-salesarray[3,2,1]
-salesarray[1,1,1]
 
-apply(salesarray,c(1),sum)
-apply(salesarray,c(2),sum)
-apply(salesarray,c(3),sum)
-apply(salesarray,c(2,3),sum)
-apply(salesarray,c(1,2,3),length)
+salesarray[3,2,1]
+salesarray[1,1,1] # salesman1, dept1, coy1, 
+salesarray[1,,] # salesman1 of all dept & copy
+salesarray[,,1] # coy1
+
+apply(salesarray, c(1), sum)  # salesman wise
+apply(salesarray, c(2), sum)  # deptwise
+apply(salesarray, c(3), sum) # coywise
+apply(salesarray, c(2,3), sum) # all salesman - dept & coywise
+apply(salesarray, c(1,2,3), length) # coywise = numbers
