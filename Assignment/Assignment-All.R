0d39e41bc028e18bb2af3815e3b219a93461f493
vijay oswalia <vijayoswalia@gmail.com>
2017-09-01 16:07:17 +0530

    AssignmentAll

--- Assignment/Assignment-All.R
+++ Assignment/Assignment-All.R
@@ -12,7 +12,8 @@ x
 mean(x)
 range(x)
 bks=seq(25,99,10)                        #breaks=seq(min,Max,10)
-bks=seq(0,100,10)
+bks=seq(0,100,10)                        # if in ()i.e (bks=seq(0,100,10), 
+                                         #it will print the value also.
 bks
 ?cut
 x.cut=cut(x,bks)
@@ -23,3 +24,59 @@ cbind(x.table)
 x
 hist(x, breaks=20)
 hist(x)
+
+
+
+
+
+
+
+x[c(1,2,3,4,5)] <- c(0,10,11,99,100)
+(bks=seq(0,100,10))
+classint1<-cut(x,breaks=bks) #defaultright=T
+head(classint1,n=5)
+head(x,n=5)
+table(classint1)
+table(classint1, useNA='ifany')
+classint2=cut(x,breaks=bks,right=F)   #default right=T
+head(classint2, n=5)
+head(x, n=5)
+table(classint2, useNA='ifany')
+
+
+#correct the problem of NA
+classint1a=cut(x, breaks=bks, include.lowest=T)  #default right
+as.character(head(classint1a, n=5))
+head(x, n=5)
+table(classint1a, useNA='ifany')
+
+
+
+classint2a=cut(x, breaks=bks, include.lowest=T, right=F)  #default 
+as.character(head(classint2a, n=5))
+head(x, n=5)
+table(classint2a, useNA='ifany')
+
+
+#Giving labels
+#length(bks)
+#levels(classint1)
+#LETTERS[1:length(bks)-1]
+#LETTERS[1:5]; letters[1:8]; month.abb[1:5]
+
+classint1b=cut(x, breaks=bks, include.lowest=T,
+              labels=LETTERS[1:length(bks)-1])    #default right
+classint1b
+
+as.character(head(classint1b , n=5))
+head(x,n=5)
+table(classint1b, useNA = 'ifany')
+
+
+
+
+x1=c(0,2,5.1,3,10,14.5,15,16,10,10,20,14.5)
+x1
+cut(x1,breaks=4, include.lowest=T)
+table(x1)
+hist(table(x1))
